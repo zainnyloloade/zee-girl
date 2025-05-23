e587b475d2cbcb8168e8826877e01b21b9fb0693
zaianb <zainnylolade@gmail.com>
2025-05-23 11:05:48 +0100

    adjues the stores

--- my-app/src/component/Experience.jsx
+++ my-app/src/component/Experience.jsx
@@ -7,7 +7,7 @@ const Experience = () => {
     <div className='pl-3 pr-10 pt-10 bg-green-600 rounded-4xl m-15'>
       <h2 className='mb-4 p-2 pb-1 text-5xl font-bold text-white'>Experience the app today!</h2>
         <div className='pt-2'>
-          <Store />
+          <Store  />
     <div className='mt-9 pl-8'>
      <img src='/download-app-phone.png' />
      </div>
