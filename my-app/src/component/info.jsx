9cbad24aea2ad912cfb2ce9c4c0cf58a21238200
zaianb <zainnylolade@gmail.com>
2025-05-23 11:26:28 +0100

    adjust d about infprmation

--- my-app/src/component/info.jsx
+++ my-app/src/component/info.jsx
@@ -1,33 +1,32 @@
-import React from 'react'
+import React from 'react';
 
 const Info = () => {
   return (
-    <div>
-      <div className='pl-3 pr-3 pt-1 m-15'>
-        <h2 className='pl-1 font-bold'>Let Us Help You</h2>
-      <div className='text-green-600'><a href="https://heyfood.medium.com/introducing-heyfood-7037d5a5f8c7?source=user_profile---------6----------------------------">About Us</a>
-         <div className='text-green-600'><a href=" https://www.linkedin.com/company/heyfood">LinkedIn</a></div>
-            <div className='text-green-600'><a href="https://heyfood.medium.com/">Blog</a></div>
-        
-      </div>
-      </div>
-
-       <div className='m-15 fex-col'>
-        <h2 className='pl-1 font-bold'>Get To Know Us</h2>
-      <div className='text-green-600'><a href="mailto:heypayapp@gmail.com">About Us</a>
-         <div className='text-green-600'><a href="https://www.heyfood.africa/">FAQs</a></div>
-            <div className='text-green-600'><a href="https://heyfood.medium.com/">Restaurants Near Me</a></div>
-      </div>
+    <div className="flex justify-between px-10 py-5 gap-8 flex-wrap">
+      {/* Column 1 */}
+      <div className="flex flex-col space-y-2">
+        <h2 className="font-bold">Let Us Help You</h2>
+        <a href="https://heyfood.medium.com/introducing-heyfood-7037d5a5f8c7?source=user_profile---------6----------------------------" className="text-green-600">About Us</a>
+        <a href="https://www.linkedin.com/company/heyfood" className="text-green-600">LinkedIn</a>
+        <a href="https://heyfood.medium.com/" className="text-green-600">Blog</a>
       </div>
 
-       <div className='m-15 fex-col'>
-        <h2 className='pl-1 font-bold'>Doing Business</h2>
-      <div className='text-green-600'><a href="https://www.heyfood.africa/driver">Become a driver</a>
-         <div className='text-green-600'><a href="https://www.heyfood.africa/merchant">Become a partner restaurant</a></div>
+      {/* Column 2 */}
+      <div className="flex flex-col space-y-2">
+        <h2 className="font-bold">Get To Know Us</h2>
+        <a href="mailto:heypayapp@gmail.com" className="text-green-600">Contact Us</a>
+        <a href="https://www.heyfood.africa/" className="text-green-600">FAQs</a>
+        <a href="https://heyfood.medium.com/" className="text-green-600">Restaurants Near Me</a>
       </div>
+
+      {/* Column 3 */}
+      <div className="flex flex-col space-y-2">
+        <h2 className="font-bold">Doing Business</h2>
+        <a href="https://www.heyfood.africa/driver" className="text-green-600">Become a Driver</a>
+        <a href="https://www.heyfood.africa/merchant" className="text-green-600">Become a Partner Restaurant</a>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Info
+export default Info;
